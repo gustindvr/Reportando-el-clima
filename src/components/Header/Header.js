aeed08c2966830fd2d4feb5340d92be0ff466b0a
Agustin <gustindvr@gmail.com>
2021-05-20 01:58:28 -0300

    styles and animations

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,30 @@
+import {Container,Row, Col } from 'react-bootstrap';
 
+import './Header.css';
 
 const Header = ({title}) => {
   return ( 
-    <>
-      <h1 
-        className='text-center m-0 p-4 bg-success color-white'>
-          <span>
-            <i 
-              className="fas fa-cloud-sun mr-4"
-            >
-            </i>
-          </span>
-        {title}
-          <span>
-            <i 
-              className="fas fa-cloud-rain ml-4">
-            </i>
-          </span>
-      </h1>
-    </>
+    <Container>
+      <Row >
+        <Col sm={3} className='m-0 p-0'>
+          <div className='tormenta clima'></div>
+          <div className='spanTitle'>El</div>
+        </Col>
+        <Col sm={3} className='m-0 p-0'>
+          <div className='lluvia clima'></div>
+          <div className='spanTitle'>Clima</div>
+        </Col>
+        <Col sm={3} className='m-0 p-0'>
+          <div className='playa clima'></div>
+          <div className='spanTitle'>Del</div>
+        </Col>
+        <Col sm={3} className='m-0 p-0'>
+          <div className='nieve clima'></div>
+          <div className='spanTitle'>Mundo</div>
+        </Col>
+      </Row>
+          <>{title}</>
+    </Container>
    );
 }
  
